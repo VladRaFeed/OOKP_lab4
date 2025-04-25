6b876ed73b7ce530e03d83bd7d1befb6d36a983e
VladRaFeed <122728801+VladRaFeed@users.noreply.github.com>
2025-04-25 11:07:16 +0300

    delete not usable files and create template for future work

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
-const { default: axios } = require('axios');
+// const { default: axios } = require('axios');
 
-const getCourses = async () => {
-  try {
-    const data = await axios.get('http://localhost:5000/getNotes');
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const getCourses = async () => {
+//   try {
+//     const data = await axios.get('http://localhost:5000/getNotes');
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-getCourses();
+// getCourses();
+
+const readBtn = document.querySelector('.readBtn');
+
+const fetchNotes = async () => {
+    try {
+      const data = await axios.get('http://localhost:5000/getNotes');
+      return data
+    } catch (error) {
+      console.log(error);
+    }
+}
+
+readBtn.addEventListener('click', fetchNotes)
+
+console.log(readBtn)
